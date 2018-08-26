c35727190138beedb76c0105cafc3330e8d30db6
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2018-08-26 19:54:31 +0100

    Tidy up summary text

--- PutridParrot.Windows/Shell/SLGP_FLAGS.cs
+++ PutridParrot.Windows/Shell/SLGP_FLAGS.cs
@@ -7,13 +7,22 @@ namespace PutridParrot.Windows
         [Flags]
         public enum SLGP_FLAGS
         {
-            /// <summary>Retrieves the standard short (8.3 format) file name</summary>
+            /// <summary>
+            /// Retrieves the standard short (8.3 format) file name
+            /// </summary>
             SLGP_SHORTPATH = 0x1,
 
-            /// <summary>Retrieves the Universal Naming Convention (UNC) path name of the file</summary>
+            /// <summary>
+            /// Retrieves the Universal Naming Convention (UNC) 
+            /// path name of the file
+            /// </summary>
             SLGP_UNCPRIORITY = 0x2,
 
-            /// <summary>Retrieves the raw path name. A raw path is something that might not exist and may include environment variables that need to be expanded</summary>
+            /// <summary>
+            /// Retrieves the raw path name. A raw path is something 
+            /// that might not exist and may include environment 
+            /// variables that need to be expanded
+            /// </summary>
             SLGP_RAWPATH = 0x4
         }
     }
